f7e5275b04aafb397353e34f5633b5bab8b7fa5e
Rest <code.rest.dev@gmail.com>
2024-01-03 19:44:57 -0300

    Version que puede guardar tareas en forma de tablas, y validar por nombre si ya se habian creado

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,52 @@
+import { useState } from "react";
 import FormTask from "./components/FormTask";
 
 function App() {
+  const [listaTareas, setListaTareas] = useState([
+    { name: "Mi primer tarea", estado: false },
+    { name: "Mi segunda tarea", estado: false },
+    { name: "Mi tercera tarea", estado: false },
+  ]);
+
+  function createTask(taskName) {
+    if (listaTareas.find((task) => task.name === taskName)) {
+      alert("La tarea ya existe");
+    } else {
+      setListaTareas([
+        ...listaTareas,
+        {
+          name: taskName,
+          estado: false,
+        },
+      ]);
+    }
+
+    // if(!listaTareas.find(task => task.name === taskName)){
+    //   setListaTareas([...listaTareas, {
+    //     name : taskName,
+    //     estado: false
+    //   }])
+    // }
+  }
+
   return (
     <div>
-      <FormTask />
+      <FormTask createTask={createTask} />
+
+      <table>
+        <thead>
+          <tr>
+            <th>Tareas</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listaTareas.map((e) => (
+            <tr key={e.name}>
+              <td>{e.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
--- src/components/FormTask.jsx
+++ src/components/FormTask.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { useState } from "react";
 
-function FormTask() {
+function FormTask({createTask}) {
   const [nombre, setNombre] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     localStorage.setItem("tarea", nombre);
     setNombre("");
+    createTask(nombre)
   };
   return (
     <form onSubmit={handleSubmit}>
